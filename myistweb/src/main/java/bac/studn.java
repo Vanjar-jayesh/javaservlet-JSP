83cdd6398708201e44c3efddf6387d1043bd2a2a
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-08-18 21:06:19 +0530

    Add files via upload

---
+++ myistweb/src/main/java/bac/studn.java
@@ -0,0 +1,35 @@
+package bac;
+
+public class studn {
+	private String name;
+	private String password;
+	
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+
+}
---
+++ myistweb/src/main/java/net/javaguides/reg/employdemo.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+package net.javaguides.reg;
+import bac.studn;
+
+public class employdemo {
+	
+
+
+}
